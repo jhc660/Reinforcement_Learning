f19f97f20d5745a488e71c7b84dec6a80ffb8000
Joel Hai Chi <joelhchi@gmail.com>
2021-01-02 23:23:30 +0800

    Update Iteration1_DQN.py

--- Deep_Q_learning/Iteration1_DQN.py
+++ Deep_Q_learning/Iteration1_DQN.py
@@ -96,7 +96,7 @@ class DQN_Agent():
                         return np.argmax(policy_net(flattenState(state))), rate, False
 
 def flattenState(tupleState):
-        #flatList = [element for tupl in tupleState for element in tupl]
+        flatList = [element for tupl in tupleState for element in tupl]
         return np.array(tupleState)
 
 def copy_weights(Copy_from, Copy_to):
